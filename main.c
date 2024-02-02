d6a525ba3a3fad69355ef97c5280ab6ea5f1c3f5
alexander <nameuserstapble@gmail.com>
2024-02-02 19:43:45 +0300

    done p tags

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 /*for entrie http request in file*/
 static uint32_t writereq( char *ptr, size_t size, size_t nmemb, FILE* data){
-	return fwrite(ptr, size, nmemb, data);
+  return fwrite(ptr, size, nmemb, data);
 }
 
 /*for define size of file*/
@@ -23,13 +23,13 @@ static uint32_t filesz(FILE* fd)
   return fsz;
 }
 
-/*openfile and verify err*/
-static void openfile(FILE** fd)
+/*openfile to read and verify errors*/
+static void opnfread(FILE** fd, const char* filename)
 {
   if(*fd > 0)
   {
-    *fd = fopen("index.html","r");
-    puts("file was open\n");
+    *fd = fopen(filename,"r");
+    puts("file was open and done to read\n");
   }
   else
   {
@@ -38,7 +38,23 @@ static void openfile(FILE** fd)
   }
 }
 
-static void writefile(char** temp_buffer, FILE** fd)
+/*openfile to write and verify errors*/ 
+static void opnfwrite(FILE** fd, const char* filename)
+{ 
+  if(*fd > 0)
+  { 
+    *fd = fopen(filename,"w");
+    puts("file was open and done to write\n");
+  }    
+  else
+  {
+    perror("error open file\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+
+static void writeresult(char** temp_buffer, FILE** fd)
 {
   puts("going writeback results\n");
 
@@ -67,72 +83,52 @@ static void buffalloc(char** buff, uint32_t fsz)
   }
 }
 
+/*функция для записи из файла в буфер*/
+static void wrtfbuff(FILE* fd, char* mem_buffer, uint32_t fsz){
+  puts("going write data into buffer\n");
+  fread(mem_buffer, sizeof(char), fsz, fd);
+}
+
 /*for find <p></p> and it record in memory buff*/
-static void cpybuffp(char* mem_buffer, char* temp_buffer, uint32_t fsz, FILE* fd)
+static void cpybuffp(char* mem_buffer, char* temp_buffer, uint32_t fsz)
 {
   int i = 0, j = 0, k = 0;
-  puts("going read file");
-  fread(mem_buffer, sizeof(char), fsz, fd);
   puts("parsing <p></p> tags...\n");
-  
-  /*
-    while(i != fsz -1)
-    {
-      if(mem_buffer[i] == '<' && mem_buffer[i + 1] == 'p')
-      {
-        puts("found entry");
-        
-        if(mem_buffer[i] == '>')
-        {
-	  puts("found >");
-	  j = i;
-          while(mem_buffer[j] != '\0')
-          {
-	    temp_buffer[k] = mem_buffer[j];
-            if((mem_buffer[j] == '<' && mem_buffer[j + 1] == '/' && mem_buffer[j + 2] == 'p' && mem_buffer[j + 3] == '>'))
-	    {
-	      break;
-	    }
-	    j++;
-	    k++;
-	  }
-        }
-      }
-      i++;
-    }      
-  */
-
-  while(i != fsz -1)
+  while(i < fsz)
   {
-    if(mem_buffer[i] == '<' && mem_buffer[i + 1] == 'p' && mem_buffer[i + 2] == '>')
+    if(mem_buffer[i] == '<' && mem_buffer[i + 1] == 'p' && mem_buffer[i + 2] == '>' && mem_buffer[i + 3] == '<')
     {
-      printf("found entry\n");
-      i = i + 3;
-      j = i;
-      k = 0;
-
-      while(mem_buffer[j] != '\0')
-      {
-        if((mem_buffer[j] == '<' && mem_buffer[j + 1] == '/' && mem_buffer[j + 2] == 'p' && mem_buffer[j + 3] == '>'))
-        {
-          break;
-        }
-
-        temp_buffer[k] = mem_buffer[j];
-        j++;
-        k++;
-      }
-
+	    i += 3;
+	    while(mem_buffer[i] != '>')
+	    {
+	      i++;
+	    }
     }
 
-      i++;
-  }
-
-  printf("\n\nparsing ended\n");
-}
+    if(mem_buffer[i] == '<' && mem_buffer[i + 1] == 'p' && mem_buffer[i + 2] == '>')
+    {
+     puts("found entry");
+     j = i;
+     j += 3;
+         
+     while(mem_buffer[j] != '\0')
+     {
+       if(mem_buffer[j] == '<' && mem_buffer[j + 1] == '/' && mem_buffer[j + 2] == 'p' && mem_buffer[j + 3] == '>')
+       {
+         break;
+       }
+       temp_buffer[k] = mem_buffer[j];
+       j++;
+       k++;
+     }
+    }
+    i++;
+   }
+}      
 
 
 /*for find <a href></a> and record it memory buffer*/
+/*
 void cpybuffhref(char* mem_buffer, char* temp_buffer, uint32_t fsize, FILE* fd)
 {
   int i = 0, j = 0, k = 0;
@@ -175,12 +171,12 @@ void cpybuffhref(char* mem_buffer, char* temp_buffer, uint32_t fsize, FILE* fd)
    }
 
     printf("\nparsing ended\n\n");	
-}
+}*/
 
 
 int main(int argc, char** argv)
 {
-  FILE* fd;
+  FILE* fd_resolv;
   FILE* fd_index;
   uint32_t fsz;
   char* mem_buffer;
@@ -188,7 +184,7 @@ int main(int argc, char** argv)
   char ch = '0';
   CURL* curl; 
   CURLcode res_curl;
-
+/*
   if(argc == 2)
   {
     puts("go opening file\n");
@@ -198,8 +194,8 @@ int main(int argc, char** argv)
     puts("paramets should be two\n");
     exit(EXIT_FAILURE);
   }
-  
-  fd_index = fopen("index.html","w");
+ 
+  opnfwrite(&fd_index, "index.html");
 
   if(fd_index == NULL)
   {
@@ -227,54 +223,23 @@ int main(int argc, char** argv)
 
   puts("Done page was downloaded\n");
   fclose(fd_index);
-  puts("you choose 1 to tags <p> 2 to tags <a href> 3 to exit from programm\n");
-  scanf("%c", &ch);
-  
-  
-  if(ch == '1')
-  {
-    openfile(&fd);
-    fsz = filesz(fd);
-    
-    buffalloc(&mem_buffer, fsz);
-    buffalloc(&temp_buffer, fsz);
-    
-    puts("memory was allocated\n");
-    printf("bytes allocated: %d\n", fsz);
-    cpybuffp(mem_buffer, temp_buffer, fsz, fd);
-    
-    puts("result was parsed\n");
-    printf("%s \n", temp_buffer);
-    
-    writefile(&temp_buffer, &fd_index);
-
-    free(mem_buffer);
-    free(temp_buffer);
-    fclose(fd);
-    fclose(fd_index);
-  }
-  else if(ch == '2')
-  {
-    openfile(&fd);
-    fsz = filesz(fd);
-
-    buffalloc(&mem_buffer, fsz);
-    buffalloc(&temp_buffer, fsz);
-
-    puts("memory was allocated\n");
-    printf("bytes allocated: %d\n", fsz);
-    cpybuffhref(mem_buffer, temp_buffer, fsz, fd);
-    
-  }
-  else if(ch == '3')
-  {
-    exit(EXIT_SUCCESS);
-  }
-  else
-  {
-    puts("uncorrect parametrs\n");
-  }
-
   curl_easy_cleanup(curl);
+  */ 
+   opnfread(&fd_index, "index.html");   
+   fsz = filesz(fd_index);
+
+   buffalloc(&mem_buffer, fsz);
+   buffalloc(&temp_buffer, fsz);
+   
+   wrtfbuff(fd_index, mem_buffer, fsz);
+   
+   fclose(fd_index);
+   
+   cpybuffp(mem_buffer, temp_buffer, fsz);
+   printf("%s\n", temp_buffer);
+   puts("--------------------------the end--------------------");
+   
+   free(mem_buffer);
+   free(temp_buffer);
   return EXIT_SUCCESS;
 }
